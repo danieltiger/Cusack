2d73475ea366263d8e6520362b1e6a14d860acde
Arik Devens <adevens@fitbit.com>
2014-06-12 19:51:27 -0700

    Initial commit of rewrite in Swift.

---
+++ Cusack/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Cusack
+//
+//  Created by Arik Devens on 6/12/14.
+//  Copyright (c) 2014 danieltiger. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
